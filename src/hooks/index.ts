8934573af5e3fc4007f914f2dd7069786123528f
j.u.p.iter <pn79122821224@gmail.com>
2019-06-01 10:25:41 +0500

    fix(hooks): exports all hooks from package

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,6 @@
 export { useShallowMergeReducer } from "./useShallowMergeReducer";
 export { useDebounce } from "./useDebounce";
+export { useListDataManager } from "./useListDataManager";
+export { useOnClickOutside } from "./useOnClickOutside";
+export { useLocalStorage } from "./useLocalStorage";
+export { useEventListener } from "./useEventListener";
